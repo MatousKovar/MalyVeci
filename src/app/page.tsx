182eb7a980c81f0029049db59140420e4d0101e8
Matouš Kovář <kovarm46@fit.cvut.cz>
2025-09-29 18:52:30 +0200

    verze 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -223,24 +223,29 @@ export default function Home() {
             Naše pětičlenná parta vznikla v roce 2024 ve Strakonicích původně jako způsob, jak trávit volný čas s nástroji v ruce. Brzy nás ale začalo lákat hrát i pro lidi – a tak jsme už během prvního roku existence odehráli 19 akcí, mezi nimi dva firemní večírky, dvě svatby, a ještě k tomu zvládli i státnice.
             </p>
             <p className="text-[#D90000] text-lg mb-3"><strong>Řídime se dvěma hlavními motty:</strong></p>            
-            <ul className="list-disc pl-6 space-y-2">
+            <ul className="list-disc pl-6 space-y-2 mb-3">
               <li><strong >Pestrý repertoár</strong> – chceme, aby si na své přišli mladší, starší, rockovější i popovější posluchači. Proto hrajeme co nejvíce různých žánrů – od Katy Perry, přes Kabáty až po Bon Jovi.</li>
-              <li><strong >Hudba nás musí bavit</strong> - pokud hudba baví kapelu, tak to baví i lidi. Naší hlavní motivací je láska k hudbě, kterou často dáváme najevou jak pohybem, tak zapálením při hraní, publikum tedy bavíme jak vizuálně, tak hudebně!</li>
+              <li><strong >Hudba nás musí bavit</strong> - pokud hudba baví kapelu, tak to baví i lidi. Naší hlavní motivací je láska k hudbě, kterou často dáváme najevou jak pohybem, tak zapálením při hraní</li>
             </ul>
+
+            <p className="mb-3">
+              Jsme technicky vybavená kapela, vlastníme jak ozvučovací soustavu, tak základní osvětlení, takže váš parket nebude nikdy vypadat nudně. Zkrátka zníme i vypadáme profesionálně.
+            </p>   
       
                   
               </div>
             {/* </p> */}
             <div className=" text-left text-stone-300 mx-auto max-w-140  leading-relaxed text-gray-100 px-4 py-4 rounded-lg shadow-lg z-10">
             <p className="mb-3">
-              Jsme technicky vybavená kapela, vlastníme jak ozvučovací soustavu, tak základní osvětlení, takže váš parket nebude nikdy vypadat nudně. Zkrátka zníme i vypadáme profesionálně. Po domluvě jsme schopní doplnit naše vystoupení i o DJ.
-            </p>      
+            Po domluvě jsme schopní doplnit naše vystoupení i o DJ a náš kytarista se postará i o plakát, který nebude jen obyčejnou pozvánkou, ale skutečně upoutá.
+            </p>
             <p className="mb-3 text-lg"> <strong className="text-[#D90000]">Proč si vybrat kapelu Malý Věci?</strong></p>
             <ul className="list-disc pl-6 space-y-2">
               <li><strong>Pestrý repertoár pro všechny generace</strong> - náš aktuální repertoár čítá přes 90 kousků, napříč žánry a etapami populární hudby. Chcete rockový koncert, retro party ve stylu 80 let, nebo jen prostě klasickou tancovačku se skladbami které znáte a milujete? Pak jsme ti praví.</li>
               {/* <li><strong>Vlastní vybavení</strong></li> */}
               <li><strong>Jsme mladá krev</strong> - naše vystoupení je stejně tak o hudbě, jako o energii a atmosféře, když hrajeme, tak prostě nevydržíme stát na místě.</li>
               <li><strong>Levnější než konkurence</strong> - hudba pro nás nikdy nebyla o penězích, proto vyjdeme vstříc i pořadatelům menších akcí. </li>
+              <li><strong>Ctíme originál</strong> - Oproti většině zábavových kapel, se snažíme přiblížit se co nejvíce originálním nahrávkám. U nás to není jen o tom zahrát pár akordů a zazpívat stejné slova. </li>
               
             </ul>
             </div>
@@ -416,11 +421,14 @@ export default function Home() {
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/6wnc03soJZURZVtyAbK81X?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/5qYKPSKeZb83S0kFskJkPJ?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/7yy1E4CAVun8vq75NMQ6FD?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/6WfhZNzOioBmDUq4xb5kvz?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/3r6AJfqJ44FepL26lwLMPf?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/2IJftBfq7pJ43tfnOR0RB3?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/273GBYX8ZWhlILVTWvQrS5?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/3k2mJZM0fdqDmW7GUbq2zs?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/29uKzagduhFDTWPCjqaGOg?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/4yKFAIgwISeVWcNnatlxx3?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/0jWgAnTrNZmOGmqgvHhZEm?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/2SiXAy7TuUkycRVbbWDEpo?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <iframe className="rounded-lg shadow-lg" src="https://open.spotify.com/embed/track/4TIJ7zSBNejpoIPaWpWRKc?utm_source=generator" width="300" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
