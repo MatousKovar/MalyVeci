a61f4a6ffdec0f12f5f3258b42850d015206538d
Matouš Kovář <kovarm46@fit.cvut.cz>
2025-09-29 18:54:42 +0200

    TYPO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -288,7 +288,7 @@ export default function Home() {
           <div className="max-w-4xl mx-auto mx-10 grid md:grid-cols-2 gap-8 px-4">
             <div className="bg-stone-800/70 p-6 rounded-xl shadow-lg z-10">
               <h3 className="text-xl font-semibold mb-2 text-red-600">
-                Rockový večer u Hlaváčů
+                Rockový večer u Hlaváčků
               </h3>
               <p className="text-gray-300 mb-2">Datum: 3.10.2025</p>
               <p className="text-gray-400">Restaurace U Hlaváčků Horažďovice</p>
