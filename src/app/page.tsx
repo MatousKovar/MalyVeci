4d91a8306d9145e4d2046bf2d2cdc4e5d0e8639b
Matous Kovar <matouskovar1@gmail.com>
2026-01-29 17:57:08 +0100

    moved data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,68 +2,13 @@
 import { useState, useEffect } from 'react';
 import React from "react";
 import Image from "next/image";
+import Navbar from '../../components/Navbar';
+import uvodka from "../../public/uvodka.jpg";
 import heroImg from "../../public/logomv.png"; 
 import heroImgMobile from "../../public/uvodka_mobil.jpeg";
-import matous from "../../public/ja.jpeg";
-import mates from "../../public/basak.jpeg";
-import jiri from "../../public/2.png";
-import roubalka from "../../public/roubalka.jpeg";
-import uvodka from "../../public/uvodka.jpg";
-import prokop from "../../public/prokop.jpeg";
-import Navbar from '../../components/Navbar';
 
-const images = [
-  "/12.PNG",
-  "/1.PNG",
-  "/jinin.jpeg",
-  "/galeriexx.jpg",
-  "/9.PNG",
-  "/15.jpeg",
-  "/jinin2.png",
-  "/houba.jpeg",
-  "/14.jpeg",
-  "/novosedly.jpeg",
-  "/4.PNG",
-  "/5.PNG",
-  "/hlavacci.jpeg",
-  "/7.PNG",
-  "/roubalka_deti.jpg",
-  "/8.PNG",
-  "/11.PNG",
-];
+import { members, images } from "@/lib/data"; // importing data
 
-const members = [
-  {
-    name: "Matouš Kovář",
-    role: "Sólová kytara",
-    bio: "Kapelník, zvukař a tahoun celé kapely. Řídí zkoušky i koncerty, dohlíží na zvuk a zároveň přidává kytarová sóla, která dávají našim vystoupením drive.",
-    image: matous,
-  },
-  {
-    name: "Jiří Bártík",
-    role: "Rytmická kytara",
-    bio: "Rytmická jistota a autor vizuálů kapely. Stará se o plakáty i grafiku, na pódiu přidává kytaru a vlastní nápady, které dodávají koncertům originální náboj.",
-    image: jiri,
-  },
-  {
-    name: "Denisa Roubalová",
-    role: "Zpěv",
-    bio: "Hlavní hlas kapely a královna pódiové energie. Stará se o sociální sítě i vizuální styl kapely a na pódiu kombinuje zpěv s tancem, takže se publikum nikdy nenudí.",
-    image: roubalka,
-  },
-  {
-    name: "Matyáš Fojtů",
-    role: "Basa",
-    bio: "Basák, bavič a moderátor večerů. Jeho rytmus drží kapelu pohromadě a jeho hlášky i improvizace baví publikum stejně jako hudba.",
-    image: mates,
-  },
-  {
-    name: "Petr Roubal",
-    role: "Bicí",
-    bio: "Metronom, který drží pevné tempo a udává energii celé kapele. Kromě bubnů má na starosti i tvorbu setlistů a s Denisou tvoří vokální duo, které dodává koncertům další rozměr.",
-    image: prokop,
-  },
-];
 
 type PosterModalProps = {
   posterSrc: string | null;  // path to the image, can be null when closed
@@ -106,7 +51,6 @@ export default function Home() {
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
-      // grows blur as you scroll (max 20px)
       setBlur(Math.min(scrollY / 50, 20));
     };
     const handleResize = () => setIsMobile(window.innerWidth < 768); 
---
+++ src/lib/data.ts
@@ -0,0 +1,60 @@
+
+import matous from "../../public/ja.jpeg";
+import mates from "../../public/basak.jpeg";
+import jiri from "../../public/2.png";
+import roubalka from "../../public/roubalka.jpeg";
+import prokop from "../../public/prokop.jpeg";
+
+
+export const images = [
+  "/12.PNG",
+  "/1.PNG",
+  "/jinin.jpeg",
+  "/galeriexx.jpg",
+  "/9.PNG",
+  "/15.jpeg",
+  "/jinin2.png",
+  "/houba.jpeg",
+  "/14.jpeg",
+  "/novosedly.jpeg",
+  "/4.PNG",
+  "/5.PNG",
+  "/hlavacci.jpeg",
+  "/7.PNG",
+  "/roubalka_deti.jpg",
+  "/8.PNG",
+  "/11.PNG",
+];
+
+export const members = [
+  {
+    name: "Matouš Kovář",
+    role: "Sólová kytara",
+    bio: "Kapelník, zvukař a tahoun celé kapely. Řídí zkoušky i koncerty, dohlíží na zvuk a zároveň přidává kytarová sóla, která dávají našim vystoupením drive.",
+    image: matous,
+  },
+  {
+    name: "Jiří Bártík",
+    role: "Rytmická kytara",
+    bio: "Rytmická jistota a autor vizuálů kapely. Stará se o plakáty i grafiku, na pódiu přidává kytaru a vlastní nápady, které dodávají koncertům originální náboj.",
+    image: jiri,
+  },
+  {
+    name: "Denisa Roubalová",
+    role: "Zpěv",
+    bio: "Hlavní hlas kapely a královna pódiové energie. Stará se o sociální sítě i vizuální styl kapely a na pódiu kombinuje zpěv s tancem, takže se publikum nikdy nenudí.",
+    image: roubalka,
+  },
+  {
+    name: "Matyáš Fojtů",
+    role: "Basa",
+    bio: "Basák, bavič a moderátor večerů. Jeho rytmus drží kapelu pohromadě a jeho hlášky i improvizace baví publikum stejně jako hudba.",
+    image: mates,
+  },
+  {
+    name: "Petr Roubal",
+    role: "Bicí",
+    bio: "Metronom, který drží pevné tempo a udává energii celé kapele. Kromě bubnů má na starosti i tvorbu setlistů a s Denisou tvoří vokální duo, které dodává koncertům další rozměr.",
+    image: prokop,
+  },
+];
